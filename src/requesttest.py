ff463270744a17d03904bc48e3ac0d4fa3efe87c
KimDeokwon <codevillain@naver.com>
2018-12-07 21:35:38 +0900

    Update request test test case

--- src/requesttest.py
+++ src/requesttest.py
@@ -2,27 +2,21 @@ from urllib import request
 import json
 import ssl
 
-def main():
-    context = ssl._create_unverified_context()
+def test():
+    ssl._create_default_https_context = ssl._create_unverified_context
     try:
-        file = open("../support/key.key", 'r')
+        file = open('../support/key.key', 'r')
         key = file.read()
         file.close()
     except:
         assert False
 
-    test(context, key)
-
-def test(context, key):
-    url = "https://www.googleapis.com/youtube/v3/search?part=id&q=jfla&type=video&key={0}".format(key)
-    req = request.urlopen(url, context=context)
+    url = 'https://www.googleapis.com/youtube/v3/search?part=id&q=jfla&type=video&key={0}'.format(key)
+    req = request.urlopen(url)
 
     assert req.getcode() == 200
 
     read = req.read()
     result = json.loads(read)
-    for temp in result["items"]:
-        print(temp["id"]["videoId"])
-
-if __name__ == "__main__":
-    main()
+    for temp in result['items']:
+        assert '' != temp['id']['videoId']
