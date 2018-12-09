181f535503c05b4f12c89bd47417c75c93729d1b
KimDeokwon <codevillain@naver.com>
2018-12-09 20:51:29 +0900

    Update AddList
    add some comments

--- src/addlist.py
+++ src/addlist.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-
 from PyQt5.QtWidgets import QWidget, QLineEdit, QGridLayout, QListView, QPushButton, QMessageBox
 from PyQt5.QtGui import QStandardItem, QStandardItemModel
 from src.notification import NotificationCenter, NotificationName
@@ -9,13 +10,14 @@ import ssl
 import threading
 
 class AddList(QWidget):
-
     def __init__(self, parent=None):
         super().__init__(parent)
+        # 클래스 기본 셋팅
         ssl._create_default_https_context = ssl._create_unverified_context
         self.query = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q={0}&maxResults=15&type=video&key=[your_youtube_api_key]'
         self.set_key_in_query()
 
+        # UI 셋팅
         self.setWindowTitle('노래 추가')
         self.main_layout = QGridLayout()
 
@@ -24,16 +26,15 @@ class AddList(QWidget):
         self.main_layout.addWidget(self.input_field, 0, 0, 1, 1)
 
         self.search_button = QPushButton('검색')
-        self.search_button.clicked.connect(self.did_clicked_search)
+        self.search_button.clicked.connect(self.did_clicked_item)
         self.main_layout.addWidget(self.search_button, 0, 1, 1, 1)
 
         self.list_view = QListView()
         self.main_layout.addWidget(self.list_view, 1, 0, 1, 2)
-        self.setLayout(self.main_layout)
 
         self.searched_data = dict()
         self.model = QStandardItemModel()
-        self.list_view.clicked.connect(self.did_clicked)
+        self.list_view.clicked.connect(self.did_clicked_item)
         self.list_view.setModel(self.model)
 
         self.indicator = LoadingIndicator(70)
@@ -41,13 +42,18 @@ class AddList(QWidget):
         self.indicator.hide()
         self.main_layout.addWidget(self.indicator, 0, 0, 2, 3)
 
+        self.setLayout(self.main_layout)
+
     def showEvent(self, event):
         self.input_field.setText("")
         self.model.removeRows(0, self.model.rowCount())
 
+    # key.key 파일에서 불러온 youtube api key값을 적용
     def set_key_in_query(self):
         try:
-            file = open("../support/key.key", 'r')
+            import os
+            path = os.getcwd() + '/support/key.key'
+            file = open(path, 'r')
             self.query = self.query.replace('[your_youtube_api_key]', file.read())
             file.close()
         except Exception as e:
@@ -56,6 +62,7 @@ class AddList(QWidget):
             print(e)
             sys.exit(-1)
 
+    # 검색한 결과를 보여줌
     def show_search_result(self):
         query = self.query.format(quote(self.input_field.text()))
         result = request.urlopen(query).read().decode('utf-8')
@@ -68,6 +75,7 @@ class AddList(QWidget):
             self.model.appendRow(QStandardItem(title))
         self.indicator.hide()
 
+    # 검색 버튼을 눌렀을 때
     def did_clicked_search(self):
         if self.input_field.text() == '':
             return
@@ -77,7 +85,8 @@ class AddList(QWidget):
         thread.daemon = True
         thread.start()
 
-    def did_clicked(self, sender):
+    # 검색한 결과에서 노래를 골랐을 때
+    def did_clicked_item(self, sender):
         key = sender.data()
         value = self.searched_data[key]
         NotificationCenter.notification(NotificationName.add_video, (key, value))
