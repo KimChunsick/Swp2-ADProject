6b95fe44f974fb81ebc0686f768b25336f27e47d
KimDeokwon <codevillain@naver.com>
2018-12-09 20:49:51 +0900

    Update Webplayer
    add some comment, now we use absolute path

--- src/webplayer.py
+++ src/webplayer.py
@@ -1,29 +1,34 @@
+#-*- coding: utf-8 -*-
 from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEngineSettings
 from PyQt5.QtWebChannel import QWebChannel
 from PyQt5.QtCore import pyqtSlot
 from src.notification import NotificationCenter, NotificationName
+import os
 
 class WebPlayer(QWebEngineView):
 
     def __init__(self, parent=None):
         super().__init__(parent)
-
-        file = open('../support/webplayer.html', 'r')
+        # 클래스 기본 설정
+        path = os.getcwd() + '/support/webplayer.html'
+        file = open(path, 'r', encoding="UTF8")
         self.html = file.read()
+        file.close()
 
+        NotificationCenter.subscribe(NotificationName.play, self.play)
         QWebEngineSettings.globalSettings().setAttribute(QWebEngineSettings.PluginsEnabled, True)
         QWebEngineSettings.globalSettings().setAttribute(QWebEngineSettings.JavascriptEnabled, True)
 
+        # UI 설정
         self.setHtml(self.html)
         self.web_channel = QWebChannel(self)
         self.web_channel.registerObject('handler', self)
         self.page().setWebChannel(self.web_channel)
-        NotificationCenter.subscribe(NotificationName.play, self.play)
 
+    # 재생
     def play(self, token):
         html = self.html.replace('{0}', token)
         self.setHtml(html)
-
 
     @pyqtSlot()
     def endVideo(self):
